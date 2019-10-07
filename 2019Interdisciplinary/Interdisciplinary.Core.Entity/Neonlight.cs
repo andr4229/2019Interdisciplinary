601914727c9faf464346b7025663ccdb7a8150ee
andr4229 <andr4229@hotmail.com>
2019-10-07 09:39:01 +0200

    Some of the entity

---
+++ 2019Interdisciplinary/Interdisciplinary.Core.Entity/Neonlight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interdisciplinary.Core.Entity
+{
+    public class Neonlight
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public Colour Color { get; set; }
+        public int MyProperty { get; set; }
+
+
+        public enum Colour
+        {
+            Red, Green, Yellow, Blue, White
+        }
+    }
+}
