714e06c5a81d960b7852bc6e8a384b66bf0d9497
andr4229 <andr4229@hotmail.com>
2019-10-07 12:35:12 +0200

    ...

--- 2019Interdisciplinary/UI.RestAPI/Startup.cs
+++ 2019Interdisciplinary/UI.RestAPI/Startup.cs
@@ -15,13 +15,14 @@ namespace UI.RestAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
-
+        public IHostingEnvironment Environment { get;}
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
