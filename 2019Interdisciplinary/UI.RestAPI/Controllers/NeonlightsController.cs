a78f02d331b839a875967fcf10a78468c8b7710f
andr4229 <andr4229@hotmail.com>
2019-10-07 09:52:03 +0200

    neonlights controller

---
+++ 2019Interdisciplinary/UI.RestAPI/Controllers/NeonlightsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.RestAPI.Controllers
+{
+    public class NeonlightsController : Controller
+    {
+        /*private readonly INeonService _neonService;
+        public NeonlightsController(INeonService neonService)
+        {
+            _neonService = neonService;
+        }*/
+        // GET api/neonlights
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/neonlights/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/neonlights
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/neonlights/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/neonlights/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
