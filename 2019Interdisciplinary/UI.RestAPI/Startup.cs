7c1b558ab7a39c02a501b46dc4ec6978a3dcb04f
andr4229 <andr4229@hotmail.com>
2019-10-07 16:35:32 +0200

    fixed so azure works

--- 2019Interdisciplinary/UI.RestAPI/Startup.cs
+++ 2019Interdisciplinary/UI.RestAPI/Startup.cs
@@ -32,6 +32,8 @@ namespace UI.RestAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             if(Environment.IsDevelopment())
             {
                 services.AddDbContext<ShopDbContext>(opt => opt.UseSqlite("Data Source=NeonlightShop.db"));
@@ -40,35 +42,39 @@ namespace UI.RestAPI
             {
                 services.AddDbContext<ShopDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
             }
+
             services.AddScoped<INeonLightRepository, NeonLightRepository>();
             services.AddScoped<INeonService, NeonService>();
+            services.AddTransient<IDbInitializer, DbInitializer>();
 
-            
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                // Initialize the database
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetService<ShopDbContext>();
+                var dbInitializer = services.GetService<IDbInitializer>();
+                dbInitializer.Initialize(dbContext);
+            }
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                using (var scope = app.ApplicationServices.CreateScope())
-                {
-                    // Initialize the database
-                    var services = scope.ServiceProvider;
-                    var dbContext = services.GetService<ShopDbContext>();
-                    var dbInitializer = services.GetService<IDbInitializer>();
-                    dbInitializer.Initialize(dbContext);
-                }
                 app.UseHsts();
-                
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             app.UseMvc();
         }
     }
